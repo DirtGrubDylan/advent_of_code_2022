4e18be1531e704fd79799874e79ca31923428556
Dylan Hicks <dman1366@gmail.com>
2022-12-02 22:32:33 -0700

    Setup day 3

---
+++ src/day_3/mod.rs
@@ -0,0 +1,12 @@
+use crate::util::file_reader::to_string_vector;
+
+pub fn run() {
+    let input = to_string_vector("inputs/day_3.txt").expect("Something went wrong with Day 3!");
+
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
