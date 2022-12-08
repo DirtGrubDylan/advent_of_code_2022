7a89616b8fde84a654d89230d3d1ccea2ed29928
Dylan Hicks <dirtgrub.dylanhicks@gmail.com>
2022-12-08 21:09:56 +0000

    Starting day 8.

---
+++ src/day_8/mod.rs
@@ -0,0 +1,17 @@
+use crate::util::file_reader::to_string_vector;
+
+pub fn run() {
+    let input = to_string_vector("inputs/day_7.txt").expect("Something went wrong Day 7!");
+
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        unimplemented!()
+    }
+}
