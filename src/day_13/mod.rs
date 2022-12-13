a8e48acdad7d8fb0d4c684e43ec639101afbcdbd
Dylan Hicks <dman1366@gmail.com>
2022-12-13 07:44:05 -0700

    Starting day 13

---
+++ src/day_13/mod.rs
@@ -0,0 +1,17 @@
+use crate::util::file_reader::to_string_vector;
+
+pub fn run() {
+    let input = to_string_vector("inputs/day_13.txt").expect("Something went wrong Day 13!");
+
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        unimplemented!()
+    }
+}
