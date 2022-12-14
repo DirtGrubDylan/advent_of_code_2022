4a82a917a6e3818a679b0be1b9a1c6275ba2253e
Dylan Hicks <dman1366@gmail.com>
2022-12-13 21:41:30 -0700

    More bp

--- src/day_13/distress.rs
+++ src/day_13/distress.rs
@@ -30,4 +30,63 @@ mod tests {
 
         assert_eq!(result, expected);
     }
+
+    #[test]
+    fn test_packet_list_of_values_from() {
+        let input: Vec<char> = "[1,110,30,1,1]".chars().collect();
+
+        let expected = Packet::List(vec![
+            Box::new(Packet::Value(1)),
+            Box::new(Packet::Value(110)),
+            Box::new(Packet::Value(30)),
+            Box::new(Packet::Value(1)),
+            Box::new(Packet::Value(1)),
+        ]);
+
+        let result = Packet::from(&input[0..]);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_packet_list_of_lists_from() {
+        let input: Vec<char> = "[[1,110,30],[1,1], []]".chars().collect();
+
+        let expected = Packet::List(vec![
+            Box::new(Packet::List(vec![
+                Box::new(Packet::Value(1)),
+                Box::new(Packet::Value(110)),
+                Box::new(Packet::Value(30)),
+            ])),
+            Box::new(Packet::List(vec![
+                Box::new(Packet::Value(1)),
+                Box::new(Packet::Value(1)),
+            ])),
+            Box::new(Packet::List(vec![])),
+        ]);
+
+        let result = Packet::from(&input[0..]);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_packet_list_of_lists_and_values_from() {
+        let input: Vec<char> = "[[1,110,30], 1, 1, []]".chars().collect();
+
+        let expected = Packet::List(vec![
+            Box::new(Packet::List(vec![
+                Box::new(Packet::Value(1)),
+                Box::new(Packet::Value(110)),
+                Box::new(Packet::Value(30)),
+            ])),
+            Box::new(Packet::Value(1)),
+            Box::new(Packet::Value(1)),
+            Box::new(Packet::List(vec![])),
+        ]);
+
+        let result = Packet::from(&input[0..]);
+
+        assert_eq!(result, expected);
+    }
 }
